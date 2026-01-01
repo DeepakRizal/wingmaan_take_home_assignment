79ce78cd4c4f302bc66b7692aac6fb8831ce030f
Deepak Rizal <deepak2000rizal@gmail.com>
2026-01-01 11:42:48 +0530

    refactor: fixed the image size problem on the welcome page

--- src/components/CharacterIllustration.js
+++ src/components/CharacterIllustration.js
@@ -3,20 +3,34 @@ import { getGenderImage } from "@/utils/genderUtils";
 
 export default function CharacterIllustration({ gender }) {
   return (
-    <div className="absolute bottom-0 left-1/2 -translate-x-1/2 flex items-end justify-center z-10 pointer-events-none">
+    <div
+      className="
+        absolute
+        bottom-0
+        left-1/2
+        -translate-x-1/2
+        z-10
+        flex
+        items-end
+        justify-center
+        w-full
+        pointer-events-none
+      "
+    >
       <Image
         src={getGenderImage(gender, "welcome")}
         alt={`${gender} character illustration`}
-        width={420}
-        height={600}
         priority
+        width={500}
+        height={750}
         className="
           object-contain
           w-auto
-          max-h-[70vh]
-          sm:max-h-[75vh]
-          md:max-h-[80vh]
-          lg:max-h-[85vh]
+          max-h-[65vh]
+          sm:max-h-[70vh]
+          md:max-h-[75vh]
+          lg:max-h-[78vh]
+          xl:max-h-[80vh]
         "
       />
     </div>
